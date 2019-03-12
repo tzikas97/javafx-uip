f00927454aaa36a610e3a8eee9b9a641e437d068
tzikas97 <tzikas97@gmail.com>
2019-03-12 23:21:32 +0200

    [+] Add week_02 optional assignment additional output

---
+++ week_02/assignment_optional/output/ShowLoginFlow/src/showflowpane/ShowFlowPane.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2019 George Tzikas <tzikas97@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package showflowpane;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+public class ShowFlowPane extends Application {
+
+    @Override
+    public void start(Stage myStage) {
+        //Δημιουργία FlowPane
+        FlowPane myPane = new FlowPane();
+        myPane.setOrientation(Orientation.VERTICAL);  //Θέστε  προσανατολισμό
+        myPane.setPadding(new Insets(10, 20, 30, 40)); //Θέστε ανω, κάτω, αριστερό και δεξί περιθώριο
+        myPane.setHgap(5);  //θέστε οριζόντιο περιθώριο μεταξύ των εικόνων
+        myPane.setVgap(5);  //θέστε κατακόρυφο περιθώριο μεταξύ των εικόνων
+
+        //Δημιουργία control nodes και τοποθέτησή τους εντος του pane
+        //Δημιουργία lblName, lblMiddleNameddleName, lblLastName
+        Label lblName = new Label("First Name:");
+        Label lblMiddleName = new Label("MI:");
+        Label lblLastName = new Label("Last Name:");
+
+        //Δημιουργία txtName, txtMiddleName, txtLastName
+        TextField txtName = new TextField();
+        TextField txtMiddleName = new TextField();
+        TextField txtLastName = new TextField();
+
+        //Προσθήκη κόμβων στο pane
+        myPane.getChildren().addAll(lblName, txtName, lblMiddleName, txtMiddleName, lblLastName, txtLastName);
+
+        //Δημιουργία σκηνικού διαστάσεων 250 χ 250 και προσθήκη του pane σε αυτό
+        Scene scene = new Scene(myPane, 250, 250);
+
+        //Προσθήκη σκηνικού στο παράθυρο
+        myStage.setScene(scene);
+
+        //Προσθήκη τίτλου "show Flow pane"
+        myStage.setTitle("show Flow pane");
+
+        //Εμφάνιση του παραθύρου
+        myStage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
---
+++ week_02/assignment_optional/output/ShowLoginGridPane/src/showlogingridpane/ShowLoginGridPane.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2019 George Tzikas <tzikas97@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package showlogingridpane;
+
+import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class ShowLoginGridPane extends Application {
+
+    @Override
+    public void start(Stage myStage) {
+        GridPane myGrid = new GridPane();
+        myGrid.setPadding(new Insets(10, 10, 10, 10));
+        myGrid.setVgap(10);
+        myGrid.setHgap(10);
+
+        Label lblName = new Label("Username:");
+        Label lblPwd = new Label("Password:");
+
+        TextField txtName = new TextField();
+        TextField txtPwd = new TextField();
+
+        txtPwd.setPromptText("password");
+
+        Button btnLogin = new Button("Log in");
+
+        GridPane.setHalignment(btnLogin, HPos.RIGHT);
+
+        // πρώτος τρόπος: χρήση μεθόδου add() για προσθήκη control
+        // και θέσης ταυτόχρονα στο grid
+        myGrid.add(lblName, 0, 0);
+
+        //προσθέστε το lblPwd στο Grid
+        myGrid.add(lblPwd, 0, 1);
+
+        //προσθέστε το txtName στο Grid
+        myGrid.add(txtName, 1, 0);
+
+        //προσθέστε το txtPwd στο Grid
+        myGrid.add(txtPwd, 1, 1);
+
+        //προσθέστε το btnLogin στο Grid
+        myGrid.add(btnLogin, 1, 2);
+
+        /*
+
+        // This needs to be commented out because of:
+        // java.lang.IllegalArgumentException: Children: duplicate children added:
+
+        // δεύτερος τρόπος: χρήση στατικής μεθόδου setConstraints()
+        GridPane.setConstraints(lblName, 0, 0);
+
+        //προσθέστε το lblPwd στο Grid
+        GridPane.setConstraints(lblPwd, 0, 1);
+
+        //προσθέστε το txtName στο Grid
+        GridPane.setConstraints(txtName, 1, 0);
+
+        //προσθέστε το txtPwd στο Grid
+        GridPane.setConstraints(txtPwd, 1, 1);
+
+        //προσθέστε το btnLogin στο Grid
+        GridPane.setConstraints(btnLogin, 1, 2);
+
+        //προσθέστε όλα τα controls στο pane
+        myGrid.getChildren().addAll(lblName, txtName, lblPwd, txtPwd);
+
+         */
+        Scene myScene = new Scene(myGrid, 250, 115);
+
+        myStage.setTitle("Login screen");
+        myStage.setScene(myScene);
+        myStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+}
