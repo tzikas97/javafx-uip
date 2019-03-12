7fea38e7b937ad80f25990b30116b69e2b3f3863
tzikas97 <tzikas97@gmail.com>
2019-03-12 05:14:39 +0200

    [+] Add week_02 required assignment output 2

---
+++ week_02/assignment_required/output/Assignment2Application2/src/assignment2application2/Assignment2Application2.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2019 George Tzikas <tzikas97@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package assignment2application2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author tzikas97
+ */
+public class Assignment2Application2 extends Application {
+
+    /**
+     *
+     * @param firstStage The application's first stage
+     */
+    @Override
+    public void start(Stage firstStage) {
+
+        firstStage.setTitle("Assignment2Application2");
+
+        // Load images in a list
+        List<ImageView> imagesList = new ArrayList<>();
+
+        Random rand = new Random();
+
+        for (int i = 0; i < 3; i++) {
+            imagesList.add(new ImageView("res/img/" + rand.nextInt(53) + ".png"));
+        }
+
+        FlowPane pane = new FlowPane();
+
+        // Layout options
+        pane.setPadding(new Insets(10, 10, 10, 10));
+        pane.setVgap(10);
+        pane.setHgap(10);
+        pane.setAlignment(Pos.CENTER);
+
+        // Add images to grid
+        pane.getChildren().addAll(imagesList);
+
+        Scene scene = new Scene(pane, 400, 200);
+
+        firstStage.setScene(scene);
+        firstStage.show();
+    }
+
+    /**
+     *
+     * @param args The command line arguments
+     */
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+}
